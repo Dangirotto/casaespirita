844c91ed82aa5e25f063c4a250f577573bd21482
Danillo Girotto <dangirotto@gmail.com>
2018-07-25 14:05:49 +0200

    Atendimento home - alterado texto

--- resources/views/atendimento/index.blade.php
+++ resources/views/atendimento/index.blade.php
@@ -15,11 +15,12 @@
             <hr>
             <p class="lead fonte-merienda">O que é o atendimento fraterno?</p>
             <div class="row">
-                <p class="col-md-8 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores autem beatae consequatur corporis cumque doloremque et expedita illo ipsum magnam maiores minima natus, nihil nostrum quam quas quibusdam quis quo repellat repellendus sed, suscipit voluptatum! A accusantium ad aperiam architecto dignissimos error eum fugit hic laudantium libero molestiae neque numquam pariatur quae qui tenetur totam, unde, vel vero voluptatibus? A dolorem est id nihil optio quae voluptate. Ad asperiores debitis distinctio dolor dolore ducimus eaque eveniet ex expedita fugit iste iusto laudantium minus neque, officia omnis quae quia quod ratione rem repellat repudiandae sit tempora tempore unde vitae voluptatum.</p>
+                <p class="col-md-8 text-justify">O atendimento fraterno é um trabalho estruturado para receber, em primeira mão, pessoas necessidades de ajuda que procuram a Doutrina Espírita, invariavelmente, como último recursos para seus males.<br>Há muitos casos, em que as pessoas procuram soluções para os seus familiares, pessoais, profissionais, enfim, conflitos diversos do dia a dia.<br>Muitas vezes céticas ou que nunca ouviram falar da Doutrina Espírita ou do Evangelho, essas pessoas precisam de estímulos para permaneceram firmes na decisão de encontrar respostas para suas perguntas.<br>E o atendimento fraterno desempenha esse papel de recepção, acolhimento e esclarecimento básico de amparo, reajuste e de redirecionamento de ideias.<br>Trata-se de atividade que deve ser feita com serenidade, disciplina e prepara, ou seja, com bom conhecimento doutrinário, e por isso nossos atendentes são selecionados entre pessoas com grande conhecimento da doutrina e experiência em atender pessoas em busca de ajuda.</p>
                 <img height="250" class="col-md-4 img-responsive" src="/images/Atendimento fraterno.png" alt="Atendimento fraterno">
             </div>
-            <hr>
+                <p class="small">Fonte: <a href="https://radioboanova.com.br/evangelho_e_reforma/qual-importancia-atendimento-fraterno-na-casa-espirita/" target="_blank">Site Rádio Boa Nova</a> (adaptado)</p>
 
+            <hr>
             <p class="lead fonte-merienda">Solicitar seu atendimento:</p>
             <p class="text-justify">O atendimento é totalmente anônimo, bastando inserir seu e-mail no campo abaixo para iniciar. Você receberá um e-mail com um link para continuar seu atendimento e receberá uma notificação quando houver resposta de um atendente.</p>
             {!! Form::open(['method'=>'post', 'action'=>'AtendimentoController@iniciar_atendimento']) !!}
